214a1a6b11b9553bcc465c0557bd4840982d0dd3
Shraefkt <74842465+Shraefkt@users.noreply.github.com>
2023-11-20 08:54:19 +0800

    Update test.py

--- main/test.py
+++ main/test.py
@@ -5,12 +5,13 @@ from utils import *
 def get_average_overhead(n_nodes,n_cover):
     counter = 0
     overhead_total = 0
+    actual_n_nodes = 2 ** get_depth(n_nodes) + n_nodes # index number
     for b in range(1,n_nodes+1):
         for a in range(1,b):
-            overcover_nodes = calc_n_cover(convert_to_index_number(a,n_nodes),convert_to_index_number(b,n_nodes),n_cover,convert_to_index_number(n_nodes,n_nodes))
-            over_number = get_number_overhead_nodes(convert_to_index_number(a,n_nodes),
-                                                        convert_to_index_number(b,n_nodes),
-                                                        convert_to_index_number(n_nodes,n_nodes),
+            overcover_nodes = calc_n_cover(convert_to_index_number(a,actual_n_nodes),convert_to_index_number(b,actual_n_nodes),n_cover,actual_n_nodes)
+            over_number = get_number_overhead_nodes(convert_to_index_number(a,actual_n_nodes),
+                                                        convert_to_index_number(b,actual_n_nodes),
+                                                        actual_n_nodes,
                                                         overcover_nodes=overcover_nodes,
                                                         debug=False)
             overhead_total += over_number / (b-a)
